e749b2ec5363af9a3ac4341e958c403bccdb778f
touremou <touremou@MOUSKAT-LAPTOP>
2020-04-30 15:05:21 +0200

    Initial commit

--- components-life-cycle/src/Modal.js
+++ components-life-cycle/src/Modal.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 class Modal extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.popUpContainer = document.createElement('div');
+        document.body.appendChild(this.popUpContainer);
+    }
+    componentWillUnmount() {
+        document.body.removeChild(this.popUpContainer);
+    }
+    
+    
     render() {
-        return (
-            <div className="modal">
-                <div>Hello World</div>
-            </div>
+        return ReactDOM.createPortal(
+            <div className="modal" onClick={this.props.close}>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, laboriosam asperiores? Minus sed quibusdam ut dolor natus. Doloremque eligendi ut quos magnam quas accusamus voluptas optio id, laborum consequatur debitis?</p>
+                    <button>Fermer</button>
+                </div>
+            </div>, this.popUpContainer
         )
     }
 }
 
-export default Modal
+export default Modal;
